ee11015bfe0871b7d7296660ffe69a04bc1de813
mtaech <mtaech@outlook.com>
2022-04-19 10:42:58 +0800

    fix:if pic title equals Info ,dont show it

--- src/bing_wallpaper/bing-pic.tsx
+++ src/bing_wallpaper/bing-pic.tsx
@@ -1,11 +1,21 @@
 import '../App.css'
 
+function PicTitle(props:{title: string | undefined}) {
+    const title = props.title;
+    if (title && title !== "Info"){
+        return(
+            <p className="bing-pic-title">{props.title}</p>
+        )
+    }else {
+        return (<p className="bing-pic-title"/>)
+    }
+}
 
 function BingPic(props: { url: string | undefined; title: string | undefined; }) {
     return(
        <div className="bing-pic-box">
            <img className="bing-pic" src={props.url} alt={props.title}/>
-           <p className="bing-pic-title">{props.title}</p>
+           <PicTitle title={props.title}/>
        </div>
     )
 }
